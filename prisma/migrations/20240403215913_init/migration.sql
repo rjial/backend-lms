19b9d4e3e5f9d94e294d4832f149316b3d2665e8
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2024-04-04 05:04:01 +0700

    fixing model

--- prisma/migrations/20240403215913_init/migration.sql
+++ prisma/migrations/20240403215913_init/migration.sql
@@ -4,11 +4,12 @@ CREATE TABLE `users` (
     `email` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
     `secretKey` VARCHAR(191) NOT NULL,
     `gender` VARCHAR(191) NOT NULL,
     `expiredAt` DATETIME(3) NULL,
     `phone` VARCHAR(191) NULL,
-    `dot` DATETIME(3) NULL,
+    `dob` DATETIME(3) NULL,
     `address` VARCHAR(191) NULL,
     `countryCode` VARCHAR(191) NULL,
     `city` VARCHAR(191) NULL,
@@ -16,6 +17,11 @@ CREATE TABLE `users` (
     `isAdmin` BOOLEAN NOT NULL DEFAULT false,
     `balance` DOUBLE NULL,
     `image` VARCHAR(191) NULL,
+    `verify` BOOLEAN NOT NULL,
+    `admin` BOOLEAN NOT NULL,
+    `created_at` DATETIME(3) NOT NULL,
+    `updated_at` DATETIME(3) NULL,
+    `deleted_at` DATETIME(3) NULL,
 
     UNIQUE INDEX `users_email_key`(`email`),
     PRIMARY KEY (`id`)
@@ -31,6 +37,18 @@ CREATE TABLE `courses` (
     `document` VARCHAR(191) NULL,
     `userCount` INTEGER NOT NULL,
     `memberCount` INTEGER NOT NULL,
+    `finished` VARCHAR(191) NOT NULL,
+    `rating5` INTEGER NOT NULL,
+    `rating4` INTEGER NOT NULL,
+    `rating3` INTEGER NOT NULL,
+    `rating2` INTEGER NOT NULL,
+    `rating1` INTEGER NOT NULL,
+    `price` VARCHAR(191) NOT NULL,
+    `level` VARCHAR(191) NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL,
+    `updated_at` DATETIME(3) NULL,
+    `deleted_at` DATETIME(3) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -41,10 +59,13 @@ CREATE TABLE `modules` (
     `courseId` INTEGER NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NOT NULL,
-    `level` VARCHAR(191) NOT NULL,
-    `score` INTEGER NOT NULL,
-    `price` DOUBLE NOT NULL,
+    `video` VARCHAR(191) NOT NULL,
+    `document` VARCHAR(191) NOT NULL,
+    `thumbnail` VARCHAR(191) NOT NULL,
     `paymentMode` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL,
+    `updated_at` DATETIME(3) NULL,
+    `deleted_at` DATETIME(3) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -52,10 +73,11 @@ CREATE TABLE `modules` (
 -- CreateTable
 CREATE TABLE `quizzes` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `courseId` INTEGER NOT NULL,
-    `name` VARCHAR(191) NOT NULL,
-    `description` VARCHAR(191) NOT NULL,
-    `rating` INTEGER NOT NULL,
+    `data` VARCHAR(191) NOT NULL,
+    `module_id` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL,
+    `updated_at` DATETIME(3) NULL,
+    `deleted_at` DATETIME(3) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -63,10 +85,12 @@ CREATE TABLE `quizzes` (
 -- CreateTable
 CREATE TABLE `events` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `courseId` INTEGER NOT NULL,
+    `user_id` INTEGER NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NOT NULL,
-    `rating` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL,
+    `updated_at` DATETIME(3) NULL,
+    `deleted_at` DATETIME(3) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -78,6 +102,9 @@ CREATE TABLE `reviews` (
     `userId` INTEGER NOT NULL,
     `rating` INTEGER NOT NULL,
     `description` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL,
+    `updated_at` DATETIME(3) NULL,
+    `deleted_at` DATETIME(3) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -86,11 +113,18 @@ CREATE TABLE `reviews` (
 CREATE TABLE `sessions` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `userId` INTEGER NOT NULL,
-    `courseId` INTEGER NOT NULL,
-    `moduleId` INTEGER NOT NULL,
-    `startedAt` DATETIME(3) NOT NULL,
-    `finishedAt` DATETIME(3) NULL,
     `score` INTEGER NULL,
+    `clientIp` VARCHAR(191) NOT NULL,
+    `userAgent` VARCHAR(191) NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `secretKey` VARCHAR(191) NOT NULL,
+    `expired` DATETIME(3) NULL,
+    `lastActivated` DATETIME(3) NULL,
+    `created_at` DATETIME(3) NOT NULL,
+    `updated_at` DATETIME(3) NULL,
+    `deleted_at` DATETIME(3) NULL,
+    `courseId` INTEGER NULL,
+    `moduleId` INTEGER NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -99,9 +133,12 @@ CREATE TABLE `sessions` (
 CREATE TABLE `assignments` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `courseId` INTEGER NOT NULL,
-    `name` VARCHAR(191) NOT NULL,
-    `description` VARCHAR(191) NOT NULL,
+    `user_id` INTEGER NOT NULL,
     `document` VARCHAR(191) NULL,
+    `video` VARCHAR(191) NULL,
+    `created_at` DATETIME(3) NOT NULL,
+    `updated_at` DATETIME(3) NULL,
+    `deleted_at` DATETIME(3) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -110,6 +147,11 @@ CREATE TABLE `assignments` (
 CREATE TABLE `categories` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `thumbnail` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL,
+    `updated_at` DATETIME(3) NULL,
+    `deleted_at` DATETIME(3) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -119,17 +161,36 @@ CREATE TABLE `course_categories` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `courseId` INTEGER NOT NULL,
     `categoryId` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL,
+    `updated_at` DATETIME(3) NULL,
+    `deleted_at` DATETIME(3) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `completions` (
+CREATE TABLE `CompletionCourses` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `created_at` DATETIME(3) NOT NULL,
+    `updated_at` DATETIME(3) NULL,
+    `deleted_at` DATETIME(3) NULL,
+    `userId` INTEGER NOT NULL,
+    `courseId` INTEGER NOT NULL,
+    `score` BIGINT NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `CompletionModules` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `created_at` DATETIME(3) NOT NULL,
+    `updated_at` DATETIME(3) NULL,
+    `deleted_at` DATETIME(3) NULL,
     `userId` INTEGER NOT NULL,
     `courseId` INTEGER NOT NULL,
     `moduleId` INTEGER NOT NULL,
-    `completedAt` DATETIME(3) NOT NULL,
+    `score` BIGINT NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -139,20 +200,40 @@ CREATE TABLE `checkouts` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `userId` INTEGER NOT NULL,
     `courseId` INTEGER NOT NULL,
+    `payment_method` VARCHAR(191) NOT NULL,
+    `verify` BOOLEAN NOT NULL,
     `createdAt` DATETIME(3) NOT NULL,
     `updatedAt` DATETIME(3) NULL,
+    `deleted_at` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Banner` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `created_at` DATETIME(3) NOT NULL,
+    `updated_at` DATETIME(3) NULL,
+    `deleted_at` DATETIME(3) NULL,
+    `user_id` INTEGER NOT NULL,
+    `course_id` INTEGER NOT NULL,
+    `video` INTEGER NOT NULL,
+    `document` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
+ALTER TABLE `courses` ADD CONSTRAINT `courses_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE `modules` ADD CONSTRAINT `modules_courseId_fkey` FOREIGN KEY (`courseId`) REFERENCES `courses`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `quizzes` ADD CONSTRAINT `quizzes_courseId_fkey` FOREIGN KEY (`courseId`) REFERENCES `courses`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `quizzes` ADD CONSTRAINT `quizzes_module_id_fkey` FOREIGN KEY (`module_id`) REFERENCES `modules`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `events` ADD CONSTRAINT `events_courseId_fkey` FOREIGN KEY (`courseId`) REFERENCES `courses`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `events` ADD CONSTRAINT `events_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `reviews` ADD CONSTRAINT `reviews_courseId_fkey` FOREIGN KEY (`courseId`) REFERENCES `courses`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -164,13 +245,16 @@ ALTER TABLE `reviews` ADD CONSTRAINT `reviews_userId_fkey` FOREIGN KEY (`userId`
 ALTER TABLE `sessions` ADD CONSTRAINT `sessions_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `sessions` ADD CONSTRAINT `sessions_courseId_fkey` FOREIGN KEY (`courseId`) REFERENCES `courses`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `sessions` ADD CONSTRAINT `sessions_courseId_fkey` FOREIGN KEY (`courseId`) REFERENCES `courses`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `sessions` ADD CONSTRAINT `sessions_moduleId_fkey` FOREIGN KEY (`moduleId`) REFERENCES `modules`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `sessions` ADD CONSTRAINT `sessions_moduleId_fkey` FOREIGN KEY (`moduleId`) REFERENCES `modules`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `assignments` ADD CONSTRAINT `assignments_courseId_fkey` FOREIGN KEY (`courseId`) REFERENCES `courses`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `assignments` ADD CONSTRAINT `assignments_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `course_categories` ADD CONSTRAINT `course_categories_courseId_fkey` FOREIGN KEY (`courseId`) REFERENCES `courses`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -179,13 +263,19 @@ ALTER TABLE `course_categories` ADD CONSTRAINT `course_categories_courseId_fkey`
 ALTER TABLE `course_categories` ADD CONSTRAINT `course_categories_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `completions` ADD CONSTRAINT `completions_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `CompletionCourses` ADD CONSTRAINT `CompletionCourses_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `completions` ADD CONSTRAINT `completions_courseId_fkey` FOREIGN KEY (`courseId`) REFERENCES `courses`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `CompletionCourses` ADD CONSTRAINT `CompletionCourses_courseId_fkey` FOREIGN KEY (`courseId`) REFERENCES `courses`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `completions` ADD CONSTRAINT `completions_moduleId_fkey` FOREIGN KEY (`moduleId`) REFERENCES `modules`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `CompletionModules` ADD CONSTRAINT `CompletionModules_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `CompletionModules` ADD CONSTRAINT `CompletionModules_courseId_fkey` FOREIGN KEY (`courseId`) REFERENCES `courses`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `CompletionModules` ADD CONSTRAINT `CompletionModules_moduleId_fkey` FOREIGN KEY (`moduleId`) REFERENCES `modules`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `checkouts` ADD CONSTRAINT `checkouts_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
